becf0b6984a8141a43f9c614e5f86d98bd0591eb
Symbat <symbataliaskar@gmail.com>
2023-03-13 14:06:09 +0600

    Initia commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pack
 
-date_hours = pack.DateTime1(2023, 12, 12, 50, 33, 120) # error case for hours
-date_hours.display_date()
+date = pack.Date1(-1, 3, 12) # error case for year
+date.display_date()
 
 date_hours = pack.DateTime1(2023, 12, 12, 23, 33, 120) # error case for seconds
 date_hours.display_date()
 
-date = pack.Date1(-1, 3, 12) # error case for year
-date.display_date()
+date_hours = pack.DateTime1(2023, 12, 12, 50, 33, 120) # error case for hours
+date_hours.display_date()
+
 
 
 
--- pack/Date.py
+++ pack/Date.py
@@ -5,12 +5,12 @@ class Date1:
         self.year = year
         self.month = month
         self.day = day
+        self.validate_date1()
 
     def display_date(self):
-        self.validate()
         print(f"{self.year:04}/{self.month:02}/{self.day:02}")
 
-    def validate(self):
+    def validate_date1(self):
         if not isinstance(self.year, int) or not 0 <= self.year <= 9999:
             raise DateTimeError("year", self.year, "Type between 0 to 9999")
         if not isinstance(self.month, int) or not 1 <= self.month <= 12:
@@ -24,10 +24,10 @@ class DateTime1(Date1):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-        self.validate()
+        self.validate_DateTime1()
 
-    def validate(self):
-        super().validate()
+    def validate_DateTime1(self):
+        super().validate_date1()
         if not isinstance(self.hours, int) or not 0 <= self.hours <= 23:
             raise DateTimeError("hours", self.hours, "Type between 0 to 23")
         if not isinstance(self.minutes, int) or not 0 <= self.minutes <= 59:
